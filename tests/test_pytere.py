10ef2775ecb1306629c30876d2fe0053a2e0f219
Astariul <remondnicola@gmail.com>
2021-10-24 15:31:51 +0900

    :white_check_mark: Add tests

---
+++ tests/test_pytere.py
@@ -0,0 +1,12 @@
+import pytest
+from pytere import is_odd
+
+
+@pytest.mark.parametrize("x", [1, 3, 7, 348973243, -1])
+def test_with_odd_numbers(x):
+    assert is_odd(x)
+
+
+@pytest.mark.parametrize("x", [0, 2, 10, 74353434, -8])
+def test_with_even_numbers(x):
+    assert not is_odd(x)

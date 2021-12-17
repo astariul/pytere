d413459efcbf9fbc0565f74afe1404c96dfe889a
remondn-swk <remondn@spacewalk.tech>
2021-12-17 19:08:46 +0900

    :sparkles: Fix dependency management

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 reqs = []
 
 extras_require = {
-    "test": ["pytest==6.2.*", "pytest-cov==3.0.*", "coverage-badge==1.0.*"],
-    "hook": ["pre-commit==2.15.*"],
-    "lint": ["isort==5.9.*", "black==21.*", "pyproject-flake8==0.0.*"],
+    "test": ["pytest~=6.2", "pytest-cov~=3.0", "coverage-badge~=1.0"],
+    "hook": ["pre-commit~=2.15"],
+    "lint": ["isort~=5.9", "black~=21.0", "pyproject-flake8~=0.0"],
 }
 extras_require["all"] = sum(extras_require.values(), [])
 extras_require["dev"] = extras_require["test"] + extras_require["hook"] + extras_require["lint"]

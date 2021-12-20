9692ded61fb4be7c88054e3dedb0c3438a7a869b
remondn-swk <remondn@spacewalk.tech>
2021-12-20 22:31:08 +0900

    :pushpin: Fix dependencies version

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ reqs = []
 extras_require = {
     "test": ["pytest~=6.2", "pytest-cov~=3.0", "coverage-badge~=1.0"],
     "hook": ["pre-commit~=2.15"],
-    "lint": ["isort~=5.9", "black~=21.9b0", "pyproject-flake8~=0.0.1a2"],
+    "lint": ["isort~=5.9", "black~=21.9b0", "pyproject-flake8~=0.0.1a2", "darglint~=1.8"],
+    "doc": ["mkdocs-material~=8.1", "mkdocstrings~=0.16"],
 }
 extras_require["all"] = sum(extras_require.values(), [])
 extras_require["dev"] = extras_require["test"] + extras_require["hook"] + extras_require["lint"]

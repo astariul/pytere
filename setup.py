7cdffc92f8c94c943733d002b97df3e68fd5df0f
Astariul <remondnicola@gmail.com>
2022-01-12 21:26:35 +0900

    :bookmark: Set version for next release

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extras_require["dev"] = (
 
 setuptools.setup(
     name="pytere",
-    version="0.1.1",
+    version="0.2.0.dev0",
     author="Nicolas REMOND",
     author_email="remondnicola@gmail.com",
     description="A Python Template Repository",

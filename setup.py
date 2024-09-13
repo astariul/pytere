78b7c63b780304c803d5375f442bcb0cc82cb65c
Astariul <43774355+astariul@users.noreply.github.com>
2024-09-14 08:43:14 +0900

    Upgrade pytest-cov

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 reqs = []
 
 extras_require = {
-    "test": ["pytest~=8.0", "pytest-cov~=4.1"],
+    "test": ["pytest~=8.0", "pytest-cov~=5.0"],
     "hook": ["pre-commit~=3.0"],
     "lint": ["ruff~=0.2"],
     "docs": ["mkdocs-material~=9.0", "mkdocstrings[python]~=0.24", "mike~=2.0"],

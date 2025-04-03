630822c01869b3ab87e161e1add48517ac04d4ae
Astariul <43774355+astariul@users.noreply.github.com>
2025-04-03 14:33:47 +0900

    📌 Better version specification

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 reqs = []
 
 extras_require = {
-    "test": ["pytest~=8.0", "pytest-cov>=5,<7"],
+    "test": ["pytest~=8.0", "pytest-cov~=6"],
     "hook": ["pre-commit~=4.0"],
     "lint": ["ruff~=0.2"],
     "docs": ["mkdocs-material~=9.0", "mkdocstrings[python]~=0.24", "mike~=2.0"],
